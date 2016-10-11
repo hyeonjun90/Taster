b7851cdd3f3140b92760666ffaa015c8e7e5288f
현준 <@jun_pc>
2016-10-11 10:01:27 +0900

    커밋

--- Taster/WebContent/common/main.jsp
+++ Taster/WebContent/common/main.jsp
@@ -7,11 +7,13 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=EUC-KR">
 <title>Insert title here</title>
-<link rel="stylesheet" href="../css/style.css" />
+<link rel="stylesheet" href="/Taster/css/style.css" />
 </head>
 <body>
 <jsp:include page="./top.jsp"></jsp:include>
-<div style="width:100%; height:300px; background-color:#fff"></div>
+<div style="width:100%; height:300px; background-color:#fff">
+	회원 등급 : <s:property value="testBean.member_level"/> <!-- 테스트 코드 -->
+</div>
 <jsp:include page="./bottom.jsp"></jsp:include>
 </body>
-</html>
+</html> 
---
+++ Taster/src/common/MainAction.java
@@ -0,0 +1,56 @@
+package common;
+
+import java.io.IOException;
+import java.io.Reader;
+
+import com.ibatis.common.resources.Resources;
+import com.ibatis.sqlmap.client.SqlMapClient;
+import com.ibatis.sqlmap.client.SqlMapClientBuilder;
+import com.opensymphony.xwork2.ActionSupport;
+
+import bean.PositionBean;
+
+public class MainAction extends ActionSupport {
+	public static Reader reader; //파일 스트림을 위한 reader
+	public static SqlMapClient sqlMapper; //SqlMapClient API를 사용하기 위한 sqlMapper 객체
+	private PositionBean testBean;
+	
+	//생성자
+	public MainAction() throws IOException {
+		reader = Resources.getResourceAsReader("sqlMapConfig.xml"); //sqlMapConfig.xml 파일의 설정내용을 가져온다.
+		sqlMapper = SqlMapClientBuilder.buildSqlMapClient(reader); //sqlMapConfig.xml의 내용을 적용한 sqlMapper 객체 생성
+		reader.close();
+	}
+		
+	public String execute() throws Exception {
+		testBean = (PositionBean) sqlMapper.queryForObject("test");
+		System.out.println("member_level() : " + testBean.getMember_level());
+		
+		return SUCCESS;
+	}
+
+	
+	
+	public PositionBean getTestBean() {
+		return testBean;
+	}
+	public void setTestBean(PositionBean testBean) {
+		this.testBean = testBean;
+	}
+	
+	
+}
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
