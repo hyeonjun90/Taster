173e73c7e1547a12a3936f1412b9be82236e97fb
은경 <@wl>
2016-10-13 09:13:17 +0900

    ww

--- Taster/src/member/MemberMyPageAction.java
+++ Taster/src/member/MemberMyPageAction.java
@@ -1,5 +1,32 @@
 package member;
 
-public class MemberMyPageAction {
+import java.io.IOException;
+import java.io.Reader;
 
+import bean.ReviewBean;
+
+import com.ibatis.common.resources.Resources;
+import com.ibatis.sqlmap.client.SqlMapClient;
+import com.ibatis.sqlmap.client.SqlMapClientBuilder;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class MemberMyPageAction extends ActionSupport {
+	public static Reader reader; //파일 스트림을 위한 reader
+	public static SqlMapClient sqlMapper; //SqlMapClient API를 사용하기 위한 sqlMapper 객체
+	private ReviewBean reviewBean;
+	
+	
+	public MemberMyPageAction() throws IOException {
+		reader = Resources.getResourceAsReader("sqlMapConfig.xml"); //sqlMapConfig.xml 파일의 설정내용을 가져온다.
+		sqlMapper = SqlMapClientBuilder.buildSqlMapClient(reader); //sqlMapConfig.xml의 내용을 적용한 sqlMapper 객체 생성
+		reader.close();
+	}
+	
+	public String form() throws Exception {
+		
+		
+		
+		
+		return SUCCESS;
+	}
 }
