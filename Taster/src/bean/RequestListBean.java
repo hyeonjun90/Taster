4f379f2d81408b8dbcda0226fa5808d71a5ec801
Chloe <Chloe@DESKTOP-466EVKG>
2016-10-14 12:04:27 +0900

    RequestListBean.java 추가

---
+++ Taster/src/bean/RequestListBean.java
@@ -0,0 +1,21 @@
+package bean;
+
+import java.sql.Date;
+
+public class RequestListBean {
+
+	private int R_IDX;
+	private Date member_joinDate; 
+	private String R_ID;
+	private String R_NICNAME;
+	private String R_SHOP_NAME;
+	private String R_SHOP_TEL;
+	private String R_SHOP_KIND;
+	private String R_SHOP_ADDR1;
+	private String R_SHOP_ADDR2;
+	private String R_SHOP_ADDR3;
+	private String R_SHOP_ADDR4;
+	private String PRICE;
+	private String HOLIDAY;
+
+}
