442e218157c310864637820b5840deffd8c8ac0a
현준 <@jun_pc>
2016-10-24 16:18:47 +0900

    커밋

---
+++ Taster/src/bean/BookmarkListBean.java
@@ -0,0 +1,41 @@
+package bean;
+
+public class BookmarkListBean {
+	private int shop_idx;
+	private String shop_name;
+	private String file_orgname;
+	private String file_savname;
+	private String member_id;
+	public int getShop_idx() {
+		return shop_idx;
+	}
+	public void setShop_idx(int shop_idx) {
+		this.shop_idx = shop_idx;
+	}
+	public String getShop_name() {
+		return shop_name;
+	}
+	public void setShop_name(String shop_name) {
+		this.shop_name = shop_name;
+	}
+	public String getFile_orgname() {
+		return file_orgname;
+	}
+	public void setFile_orgname(String file_orgname) {
+		this.file_orgname = file_orgname;
+	}
+	public String getFile_savname() {
+		return file_savname;
+	}
+	public void setFile_savname(String file_savname) {
+		this.file_savname = file_savname;
+	}
+	public String getMember_id() {
+		return member_id;
+	}
+	public void setMember_id(String member_id) {
+		this.member_id = member_id;
+	}
+	
+	
+}
