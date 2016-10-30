e302d8e53b6b92eaf9d2cda214ea22e347726fd7
USER <USER@Chloe-13U360>
2016-10-30 23:59:06 +0900

    추천리스트 추가

--- Taster/WebContent/foods/foods_recommend_list.jsp
+++ Taster/WebContent/foods/foods_recommend_list.jsp
@@ -1,13 +1,103 @@
 <%@ page language="java" contentType="text/html; charset=EUC-KR"
     pageEncoding="EUC-KR"%>
+<%@ taglib prefix="s" uri="/struts-tags" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=EUC-KR">
 <title>Insert title here</title>
-<link rel="stylesheet" href="/Taster/css/style.css" />
+<link rel="stylesheet" href="/Taster/css/style.css" type="text/css"> 
+
+<script type="text/javascript">
+	function open_win_noresizable (url, name) {
+		var oWin = window.open(url, name, "scrollbars=no, status=no, resizable=no, width=300, height=150");		
+	}
+	
+	$(function(){
+		var $magazineDelete = $('.magazineDelete');
+
+		$magazineDelete.each(function(){
+			var $btn = $(this).children('.deleteMagazine');
+
+			$btn.on('click',function(){
+				var check=confirm("매거진을 삭제하시겠습니까?")
+				if(check){
+					true;
+				}
+				else{
+					return false;
+				}
+			});
+		});
+	});
+</script>
+
+<style>
+	.RecomViewBlock {font-family : 'Noto Sans', sans-serif; align:center; width:70%; margin:150px; text-align:center; border:3px solid orange;}
+	.RecommendView { margin:150px; float:center; width:97%; margin-top:20px; border:2px solid #d5d5d5;  margin:10px;}
+	.reviewContent {margin-left:10px; padding: 3px; font-family : 'Noto Sans', sans-serif; }
+	.rec_idx {padding: 10px; width:100%; font-size: 12px; float:left; text-align:left; margin:10px;}
+	.rec_date {float:right; width:30%; text-align:right; color:#a6a6a6; font-size:13px; height:20px;margin:10px;}
+	.rec_subject {float:left; width: 60%; float:left;text-align:left;  font-weight:bold; font-size:15px; height:20px; margin:10px; }
+	.content { font-size: 30px; color:#555; font-weight:bold; margin:10px;}
+	.rec_image { width:80%; float:center; text-align:center;  margin:10px;}
+	. rec_content { width:80%; float:center; font-size: 15px; color:#555; font-weight:bold; margin:10px;}
+	
+	
+</style>
+
 </head>
 <body>
+
+<div class="RecomViewBlock" style="float:center;">
+
+	<table width="800" align="center" style="color:#a6a6a6" cellspacing="0" cellpadding="2">
+		<tr>
+			<td align="center"><h1>Editor's Pick!</h1> <br>
+			<h4>에디터가 추천하는, 요즘 핫한 바로 그 맛집!</h4></td>
+		</tr>
+		
+	</table>
+    <s:if test="#session.member_level == 3">
+   <!-- 글쓰기 버튼 -->
+   	<table width="800">
+  	 <tr align="right" >
+  	  <td>
+         <input name="list" type="button" value="전체글 보기" class="inputb" onClick="javascript:location.href='adminRboardList.action?currentPage=<s:property value="currentPage" />';" />
+         <input name="write" type="button" value="새글 올리기" class="inputb" onClick="javascript:location.href='rboardWriteForm.action?currentPage=<s:property value="currentPage" />';" />
+      </td>
+   </tr></table>
+   </s:if>
+  	
+
+	<!-- 리뷰글 반복출력 -->
+	<c:forEach items="${RecomList}" var="RecomList" varStatus="status">
+	
+		<!-- 개별 뷰 블록 -->
+		
+		<div class="RecommendView">
+		 	<div id="rec_idx" class="rec_idx">No: ${RecomList.rec_idx}</div><!-- 글번호 -->
+		 	<div id="rec_date" class="rec_date">작성일:${RecomList.rec_date}</div><!-- 작성일 -->
+	
+		 	<div id="rec_subject" class="rec_subject">제목 : ${RecomList.rec_subject}</div><!-- 제목 -->
+		 	
+		 	<div style="clear:both; width:100%;float:left; border:1px solid #d5d5d5;"></div> <!-- 구분선 -->
+		 	
+		 		<div id="content">
+		 			<p class="rec_image"><img src="/Taster/images/${RecomList.rec_image}"/></p><!-- 이미지 -->
+					<p class="rec_content" >${RecomList.rec_content}</p>		<!-- 본문내용 -->
+				</div>
+		
+		</div>
+	</c:forEach>
+	
+
+
+</div>
+
+
 
 </body>
 </html>
--- Taster/src/rboard/AdminRBoardViewAction.java
+++ Taster/src/rboard/AdminRBoardViewAction.java
@@ -1,5 +1,152 @@
 package rboard;
 
-public class AdminRBoardViewAction {
+import com.opensymphony.xwork2.ActionSupport;
+import com.ibatis.common.resources.Resources;
+import com.ibatis.sqlmap.client.SqlMapClient;
+import com.ibatis.sqlmap.client.SqlMapClientBuilder;
+
+import java.io.Reader;
+import java.util.ArrayList;
+/*import java.io.File;
+import java.io.FileInputStream;*/
+import java.io.InputStream;
+import java.io.IOException;
+
+import bean.FoodsDetailBean;
+
+/*import java.net.URLEncoder;*/
+
+import bean.RecomBoardBean;
+
+public class AdminRBoardViewAction extends ActionSupport {
+	
+
+	public static Reader reader;
+	public static SqlMapClient sqlMapper;
+	
+	private RecomBoardBean RCBean = new RecomBoardBean();
+	//private ArrayList<RecomBoardBean> RecomList = new ArrayList<RecomBoardBean>(); //리뷰(코멘트) 리스트
+	
+	//private RecomBoardBean resultClass = new RecomBoardBean();
+	
+	private int currentPage;
+	private int rec_idx;
+	
+	private InputStream inputStream;
+	private String contentDiposition;
+	private long contentLength;
+	
+	public AdminRBoardViewAction() throws IOException {
+		
+		reader = Resources.getResourceAsReader("sqlMapConfig.xml");
+		sqlMapper = SqlMapClientBuilder.buildSqlMapClient(reader);
+		reader.close();
+	}
+	
+	// 상세보기
+	public String execute() throws Exception {
+/*		// 해당 글의 조회수 +1.
+		paramClass.setR_idx(getR_idx());
+		sqlMapper.update("updateReadHit", paramClass);*/
+		// 해당 번호의 글을 가져온다.
+		RCBean = (RecomBoardBean) sqlMapper.queryForObject("rboard-selectOne", getRec_idx());
+		//RecomList = (ArrayList<RecomBoardBean>)sqlMapper.queryForList("recommandList", getRec_idx());
+		
+		return SUCCESS;
+	}
+	
+	// 첨부 파일 다운로드
+	/*public String download() throws Exception {
+		
+		// 해당 번호의 파일 정보를 가져온다.
+		resultClass = (RequestListBean) sqlMapper.queryForObject("selectOne", getR_idx());
+		
+		// 파일 경로와 파일명을 file 객체에 넣는다.
+		File fileInfo = new File(fileUploadPath + resultClass.getFile_savname());
+		
+			// 다운로드 파일 정보 설정.
+		setContentLength(fileInfo.length());
+		setContentDisposition("attachement;filename=" +URLEncoder.encode(resultClass.getFile_orgname(), "EUC-KR"));
+		setInputStream(new FileInputStream(fileUploadPath + resultClass.getFile_savname()));
+		return SUCCESS;
+	}*/
+
+	public static Reader getReader() {
+		return reader;
+	}
+
+	public static void setReader(Reader reader) {
+		AdminRBoardViewAction.reader = reader;
+	}
+
+	public static SqlMapClient getSqlMapper() {
+		return sqlMapper;
+	}
+
+	public static void setSqlMapper(SqlMapClient sqlMapper) {
+		AdminRBoardViewAction.sqlMapper = sqlMapper;
+	}
+
+	
+	/*public ArrayList<RecomBoardBean> getRecomList() {
+		return RecomList;
+	}
+
+	public void setRecomList(ArrayList<RecomBoardBean> recomList) {
+		RecomList = recomList;
+	}*/
+
+	public RecomBoardBean getRCBean() {
+		return RCBean;
+	}
+	
+
+	public void setRCBean(RecomBoardBean rCBean) {
+		RCBean = rCBean;
+	}
+
+	public int getCurrentPage() {
+		return currentPage;
+	}
+
+	public void setCurrentPage(int currentPage) {
+		this.currentPage = currentPage;
+	}
+
+	public int getRec_idx() {
+		return rec_idx;
+	}
+
+	public void setRec_idx(int rec_idx) {
+		this.rec_idx = rec_idx;
+	}
+
+	public InputStream getInputStream() {
+		return inputStream;
+	}
+
+	public void setInputStream(InputStream inputStream) {
+		this.inputStream = inputStream;
+	}
+
+	public String getContentDiposition() {
+		return contentDiposition;
+	}
+
+	public void setContentDiposition(String contentDiposition) {
+		this.contentDiposition = contentDiposition;
+	}
+
+	public long getContentLength() {
+		return contentLength;
+	}
+
+	public void setContentLength(long contentLength) {
+		this.contentLength = contentLength;
+	}
+	
+
+	
+	
 
 }
